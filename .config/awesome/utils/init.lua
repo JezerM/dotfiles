07bfbf125e5a1925d81f4a3b7c464b2521305830
JezerM <amyuki4@icloud.com>
2022-01-19 23:03:03 -0600

    Removed awesomewm config...

--- .config/awesome/utils/init.lua
+++
@@ -1,9 +0,0 @@
-return {
-    layoutbox = require("utils.layoutbox");
-    powerline = require("utils.powerline");
-    brightness = require("utils.brightness");
-    dock = require("utils.dock");
-    filesystem = require("utils.file");
-    battery = require("utils.battery");
-    files = require("utils.file");
-}
--- .config/awesome/utils/sidebar.lua
+++
@@ -1,1 +0,0 @@
-local gears = require("gears")
