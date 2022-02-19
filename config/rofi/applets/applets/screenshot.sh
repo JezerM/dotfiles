b1252280930bbe7abb42800a077bfc893bb4334e
JezerM <amyuki4@icloud.com>
2022-02-19 11:19:44 -0600

    Save screenshot to PICTURES/Screenshots if available

--- config/rofi/applets/applets/screenshot.sh
+++ config/rofi/applets/applets/screenshot.sh
@@ -12,7 +12,7 @@ rofi_command="rofi -theme $dir/screenshot.rasi"
 
 # Error msg
 msg() {
-	rofi -theme "$HOME/.config/rofi/applets/styles/message.rasi" -e "Please install 'scrot' first."
+  rofi -theme "$HOME/.config/rofi/applets/styles/message.rasi" -e "Please install 'scrot' first."
 }
 
 # Options
@@ -22,29 +22,33 @@ window=""
 
 # Variable passed to rofi
 options="$screen\n$area\n$window"
+target="$(xdg-user-dir PICTURES)/Screenshots"
+if [ ! -d "$target" ]; then
+  target="$(xdg-user-dir PICTURES)"
+fi
 
 chosen="$(echo -e "$options" | $rofi_command -p 'scrot' -dmenu -selected-row 1)"
 case $chosen in
-    $screen)
-		if [[ -f /usr/bin/scrot ]]; then
-			sleep 1; scrot 'Screenshot_%Y-%m-%d-%S_$wx$h.png' -e 'mv $f $$(xdg-user-dir PICTURES) ; viewnior $$(xdg-user-dir PICTURES)/$f'
-		else
-			msg
-		fi
-        ;;
-    $area)
-		if [[ -f /usr/bin/scrot ]]; then
-			scrot -s 'Screenshot_%Y-%m-%d-%S_$wx$h.png' -e 'mv $f $$(xdg-user-dir PICTURES) ; viewnior $$(xdg-user-dir PICTURES)/$f'
-		else
-			msg
-		fi
-        ;;
-    $window)
-		if [[ -f /usr/bin/scrot ]]; then
-			sleep 1; scrot -u 'Screenshot_%Y-%m-%d-%S_$wx$h.png' -e 'mv $f $$(xdg-user-dir PICTURES) ; viewnior $$(xdg-user-dir PICTURES)/$f'
-		else
-			msg
-		fi
-        ;;
+  $screen)
+    if [[ -f /usr/bin/scrot ]]; then
+      sleep 1; scrot 'Screenshot_%Y-%m-%d-%S_$wx$h.png' -e "mv \$f ${target}/\$f"
+    else
+      msg
+    fi
+    ;;
+  $area)
+    if [[ -f /usr/bin/scrot ]]; then
+      scrot -s 'Screenshot_%Y-%m-%d-%S_$wx$h.png' -e "mv \$f ${target}/\$f"
+    else
+      msg
+    fi
+    ;;
+  $window)
+    if [[ -f /usr/bin/scrot ]]; then
+      sleep 1; scrot -u 'Screenshot_%Y-%m-%d-%S_$wx$h.png' -e "mv \$f ${target}/\$f"
+    else
+      msg
+    fi
+    ;;
 esac
 
