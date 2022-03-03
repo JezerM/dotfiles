649957cec7a41a88bf573e2480df619bfec74a74
JezerM <amyuki4@icloud.com>
2022-03-03 16:14:00 -0600

    Add vim spell and replace some tabs with spaces

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -27,6 +27,7 @@ require("packer").startup(function()
   use "hrsh7th/cmp-buffer"
   use "hrsh7th/cmp-path"
   use "hrsh7th/cmp-calc"
+  use "f3fora/cmp-spell"
   use "ray-x/cmp-treesitter"
   use "quangnguyen30192/cmp-nvim-ultisnips"
   --use {'tzachar/cmp-tabnine', run='./install.sh', requires = 'hrsh7th/nvim-cmp'}
--- config/nvim/lua/completion.lua
+++ config/nvim/lua/completion.lua
@@ -4,52 +4,54 @@ local cmp = require'cmp'
 local lspkind = require "lspkind"
 
 cmp.setup({
-		snippet = {
-			expand = function(args)
-				vim.fn["UltiSnips#Anon"](args.body)
-			end
-		},
-		mapping = {
-			['<C-d>'] = cmp.mapping.scroll_docs(-4),
-			['<C-f>'] = cmp.mapping.scroll_docs(4),
-			['<C-Space>'] = cmp.mapping.complete(),
-			['<C-e>'] = cmp.mapping.close(),
-			['<CR>'] = cmp.mapping.confirm({
-				select = false,
-				behavior = cmp.ConfirmBehavior.Replace,
-			}),
-		},
-		sources = {
-			--{ name = 'cmp_tabnine' },
-			{ name = 'nvim_lua' },
-			{ name = 'nvim_lsp' },
-			{ name = 'ultisnips' },
-			{ name = 'calc' },
-			{ name = 'buffer' },
-			{ name = 'path' },
-			--{ name = 'treesitter' },
-		},
-		completion = {
-			keyword_length = 1,
-			autocomplete = true,
-		},
-		formatting = {
-			format = lspkind.cmp_format({with_text = false, menu = ({
-				buffer = "[Buffer]",
-				calc = "[Calc]",
-				nvim_lsp = "[LSP]",
-				luasnip = "[LSnippet]",
-				ultisnips = "[USnippet]",
-				nvim_lua = "[Lua]",
-				latex_symbols = "[Latex]",
-				treesitter = "[TreeS]",
-				cmp_tabnine = "[Nine]",
-			})}),
-		},
-		experimental = {
-			native_menu = true,
-			ghost_text = true,
-		},
+        snippet = {
+            expand = function(args)
+                vim.fn["UltiSnips#Anon"](args.body)
+            end
+        },
+        mapping = {
+            ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+            ['<C-f>'] = cmp.mapping.scroll_docs(4),
+            ['<C-Space>'] = cmp.mapping.complete(),
+            ['<C-e>'] = cmp.mapping.close(),
+            ['<CR>'] = cmp.mapping.confirm({
+                select = false,
+                behavior = cmp.ConfirmBehavior.Replace,
+            }),
+        },
+        sources = {
+            --{ name = 'cmp_tabnine' },
+            { name = 'nvim_lua' },
+            { name = 'nvim_lsp' },
+            { name = 'ultisnips' },
+            { name = 'calc' },
+            { name = 'path' },
+            { name = 'spell' },
+            { name = 'buffer' },
+            --{ name = 'treesitter' },
+        },
+        completion = {
+            keyword_length = 1,
+            autocomplete = true,
+        },
+        formatting = {
+            format = lspkind.cmp_format({with_text = false, menu = ({
+                buffer = "[Buffer]",
+                calc = "[Calc]",
+                spell = "[Spell]",
+                nvim_lsp = "[LSP]",
+                luasnip = "[LSnippet]",
+                ultisnips = "[USnippet]",
+                nvim_lua = "[Lua]",
+                latex_symbols = "[Latex]",
+                treesitter = "[TreeS]",
+                cmp_tabnine = "[Nine]",
+            })}),
+        },
+        experimental = {
+            native_menu = true,
+            ghost_text = true,
+        },
 })
 
 local cmp_autopairs = require('nvim-autopairs.completion.cmp')
