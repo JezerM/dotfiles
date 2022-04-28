866b09696b8ad056acb4b62c04c033a621890a71
Jezer Mejía <amyuki4@icloud.com>
2022-04-28 11:16:02 -0600

    Use lua-language-server binary instead of path to binary

--- config/nvim/lua/settings.lua
+++ config/nvim/lua/settings.lua
@@ -187,17 +187,6 @@ lspconfig.texlab.setup {
     end
 }
 
-require("lspfuzzy").setup{}
-
-require("colorizer").setup({
-    "*";
-}, {
-    RGGBBAA = true;
-    RRGGBB = true;
-    names = false;
-    css = true;
-})
-
 lspconfig.tsserver.setup {
     filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
     on_attach = function(client, bufnr)
@@ -321,52 +310,47 @@ require("shade").setup({
   }
 })
 
-local system_name
-if vim.fn.has("mac") == 1 then
-    system_name = "macOS"
-elseif vim.fn.has("unix") == 1 then
-    system_name = "Linux"
-elseif vim.fn.has('win32') == 1 then
-    system_name = "Windows"
-else
-    print("Unsupported system for sumneko")
-end
+require("lspfuzzy").setup{}
 
--- set the path to the sumneko installation; if you previously installed via the now deprecated :LspInstall, use
-local sumneko_root_path = "/home/jezer/Descargas/lua-language-server"
-local sumneko_binary = sumneko_root_path.."/bin/"..system_name.."/lua-language-server"
+require("colorizer").setup({
+    "*";
+}, {
+    RGGBBAA = true;
+    RRGGBB = true;
+    names = false;
+    css = true;
+})
 
 local runtime_path = vim.split(package.path, ';')
 table.insert(runtime_path, "lua/?.lua")
 table.insert(runtime_path, "lua/?/init.lua")
 
 lspconfig.sumneko_lua.setup {
-    cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"};
     settings = {
         Lua = {
-        runtime = {
-            -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-            version = 'LuaJIT',
-            -- Setup your lua path
-            path = runtime_path,
-        },
-        diagnostics = {
-            -- Get the language server to recognize the `vim` global
-            globals = {'vim', "awesome"},
-        },
-        workspace = {
-            -- Make the server aware of Neovim runtime files
-            library = merge_table(
-                    vim.api.nvim_get_runtime_file("", true),
-                    {
-                        "/usr/share/awesome/lib/"
-                    }
-                ),
-        },
-        -- Do not send telemetry data containing a randomized but unique identifier
-        telemetry = {
-            enable = false,
-        },
+            runtime = {
+                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                version = 'LuaJIT',
+                -- Setup your lua path
+                path = runtime_path,
+            },
+            diagnostics = {
+                -- Get the language server to recognize the `vim` global
+                globals = {'vim', "awesome"},
+            },
+            workspace = {
+                -- Make the server aware of Neovim runtime files
+                library = merge_table(
+                        vim.api.nvim_get_runtime_file("", true),
+                        {
+                            "/usr/share/awesome/lib/"
+                        }
+                    ),
+            },
+            -- Do not send telemetry data containing a randomized but unique identifier
+            telemetry = {
+                enable = false,
+            },
         },
     },
     on_attach = function(client, bufnr)
