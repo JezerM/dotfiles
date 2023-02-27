771fe077de65124b9973e23b49d523509f603013
Jezer Mejía <amyuki4@icloud.com>
2023-02-27 16:51:43 -0600

    Enabled signatureHelp for jdtls

--- config/nvim/lua/lsp-config.lua
+++ config/nvim/lua/lsp-config.lua
@@ -380,9 +380,14 @@ lspconfig.phpactor.setup {
 }
 lspconfig.jdtls.setup{
     cmd = { 'jdtls' },
+    settings = {
+        java = {
+            signatureHelp = { enabled = true }
+        }
+    },
     on_attach = function(client, bufnr)
         client.server_capabilities.documentFormattingProvider = false
-        client.server_capabilities.hoverProvider = false
+        client.server_capabilities.hoverProvider = true
         on_attach(client, bufnr)
     end
 }
