cbe4bd490acb649aa5770238f65f41ce93a14093
Jezer Mejía <amyuki4@icloud.com>
2023-03-03 18:33:50 -0600

    Change sumneko_lua to lua_ls

--- config/nvim/lua/lsp-config.lua
+++ config/nvim/lua/lsp-config.lua
@@ -409,22 +409,16 @@ lspconfig.dockerls.setup{
     end
 }
 
-local runtime_path = vim.split(package.path, ';')
-table.insert(runtime_path, "lua/?.lua")
-table.insert(runtime_path, "lua/?/init.lua")
-
-lspconfig.sumneko_lua.setup {
+lspconfig.lua_ls.setup {
     settings = {
         Lua = {
             runtime = {
                 -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-                version = 'LuaJIT',
-                -- Setup your lua path
-                path = runtime_path,
+                version = "LuaJIT",
             },
             diagnostics = {
                 -- Get the language server to recognize the `vim` global
-                globals = {'vim', "awesome"},
+                globals = {"vim", "awesome"},
             },
             workspace = {
                 -- Make the server aware of Neovim runtime files
