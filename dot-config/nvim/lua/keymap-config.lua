e12a9e9af945c9fc0b6a7ac5b986d86b0a736ae9
Jezer Mejía <amyuki4@icloud.com>
2024-06-30 09:57:42 -0600

    Update keymaps

--- dot-config/nvim/lua/keymap-config.lua
+++ dot-config/nvim/lua/keymap-config.lua
@@ -2,6 +2,7 @@ vim.g.mapleader = ","
 
 vim.keymap.set({ "n", "v" }, "<Leader>y", "\"+y", { desc = "Yank to clipboard" })
 vim.keymap.set("n", "<Leader>Y", "\"+Y", { desc = "Yank whole line" })
+vim.keymap.set("n", "<Leader>p", "\"+p", { desc = "Paste from clipboard" })
 
 vim.keymap.set({ "n", "v" }, "<Leader>d", "\"_d", { desc = "Delete without yank" })
 
