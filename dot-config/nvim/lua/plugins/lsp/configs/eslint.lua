c6fdfb02775285b36af36bf6c5c8967243e9f2d2
Jezer Mejía <amyuki4@icloud.com>
2025-08-04 00:43:29 -0600

    Delay eslint

--- dot-config/nvim/lua/plugins/lsp/configs/eslint.lua
+++ dot-config/nvim/lua/plugins/lsp/configs/eslint.lua
@@ -1,3 +1,11 @@
 return {
     filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx", "vue", "astro", "typescript.glimmer", "javascript.glimmer" },
+    settings = {
+        format = true,
+        quiet = true,
+        run = "onSave",
+    },
+    flags = {
+        debounce_text_changes = 5000
+    }
 }
